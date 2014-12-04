8209682c8fc03b9256e23b5b4621b90f244896b5
Dennisbonke <dennisb@kabelfoon.net>
2014-12-04 11:16:05 +0100

    Updated main class.

--- src/main/java/com/dennisbonke/dblib/DBLib.java
+++ src/main/java/com/dennisbonke/dblib/DBLib.java
@@ -2,6 +2,7 @@ package com.dennisbonke.dblib;
 
 import com.dennisbonke.dblib.reference.Reference;
 import com.dennisbonke.dblib.util.LogHelper;
+import cpw.mods.fml.common.Loader;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
@@ -9,6 +10,21 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION, canBeDeactivated = false)
 public class DBLib {
+    {
+        if (Loader.isModLoaded("Advanced Foods")) {
+            LogHelper.info("AF found, stay tuned!");
+        } else {
+            LogHelper.info("AF not found :(");
+        }
+    }
+
+    {
+        if (Loader.isModLoaded("Advanced Tools")) {
+            LogHelper.info("AT found, stay tuned!");
+        } else {
+            LogHelper.info("AT not found :(");
+        }
+    }
     @Mod.Instance(Reference.MOD_ID)
     public static DBLib instance;
 
