d4a04803a9b50d2d95a233796fa75d5d5d7bf92d
Dennisbonke <dennisb@kabelfoon.net>
2014-12-04 14:25:13 +0100

    Added missing Pulse class.

---
+++ src/main/java/com/dennisbonke/dblib/pulsar/config/IConfiguration.java
@@ -0,0 +1,32 @@
+package com.dennisbonke.dblib.pulsar.config;
+
+// Credits SlimeKing
+
+import com.dennisbonke.dblib.pulsar.pulse.PulseMeta;
+
+/**
+ * Interface for config handlers.
+ *
+ * @author Arkan <arkan@drakon.io>
+ */
+
+public interface IConfiguration {
+
+    /**
+     * Perform any configuration loading required.
+     */
+    public void load();
+
+    /**
+     * Gets whether the given module is enabled in the config.
+     *
+     * @param meta The pulse metadata.
+     * @return Whether the module is enabled.
+     */
+    public boolean isModuleEnabled(PulseMeta meta);
+
+    /**
+     * Flush configuration to disk/database/whatever.
+     */
+    public void flush();
+}
