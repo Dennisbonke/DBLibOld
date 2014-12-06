479e95a9b38608b237df8f349b6c6a93dfc5e449
Dennisbonke <dennisb@kabelfoon.net>
2014-12-06 15:09:27 +0100

    Updated Reference and build.gradle file

--- src/main/java/com/dennisbonke/dblib/reference/Reference.java
+++ src/main/java/com/dennisbonke/dblib/reference/Reference.java
@@ -1,14 +1,22 @@
 package com.dennisbonke.dblib.reference;
 
 public class Reference {
+
+// Names
 public static final String MOD_ID = "dblib";
 public static final String MOD_NAME = "DBLib";
-    //public static final String COMMON = "com.dennisbonke.dblib.Proxy";
-    //public static final String CLIENT = "com.dennisbonke.dblib.client.ProxyClient";
 
-public static final int MAJOR = 0, MINOR = 0, REVISION = 1;
+// Versions
+public static final String MC_VERSION = "1.7.10";
+public static final String FORGE_BUILD = "10.13.0.1207";
+public static final int MAJOR = 0, MINOR = 1, REVISION = 0;
+public static final String VERSION = MC_VERSION + "-" + MAJOR + "." + MINOR + "." + REVISION;
+public static final String FORGE_VERSION = MC_VERSION + "-" + FORGE_BUILD;
 
-public static final String VERSION = "1.7.10" + "-" + MAJOR + "." + MINOR + "." + REVISION;
+// Proxies
+//public static final String COMMON_PROXY = "com.dennisbonke.dblib.Proxy";
+//public static final String CLIENT_PROXY = "com.dennisbonke.dblib.client.ProxyClient";
 
-//public static final String UPDATE_URL = "http://files.lomeli12.net/MinecraftMods/LomLib/update.json";
+// Update
+//public static final String UPDATE_URL = "http://play.zsinfo.nl/MinecraftMods/DBLib/update.json";
 }
