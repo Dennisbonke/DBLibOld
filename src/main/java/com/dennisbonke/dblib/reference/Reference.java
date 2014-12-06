26b18b2a83be5219b509ec9fcc62ef45dfd375d0
Dennisbonke <dennisb@kabelfoon.net>
2014-12-06 15:12:10 +0100

    Updated mcmod.info, Reference and build.gradle file.

--- src/main/java/com/dennisbonke/dblib/reference/Reference.java
+++ src/main/java/com/dennisbonke/dblib/reference/Reference.java
@@ -8,7 +8,7 @@ public static final String MOD_NAME = "DBLib";
 
 // Versions
 public static final String MC_VERSION = "1.7.10";
-public static final String FORGE_BUILD = "10.13.0.1207";
+public static final String FORGE_BUILD = "10.13.0.1240";
 public static final int MAJOR = 0, MINOR = 1, REVISION = 0;
 public static final String VERSION = MC_VERSION + "-" + MAJOR + "." + MINOR + "." + REVISION;
 public static final String FORGE_VERSION = MC_VERSION + "-" + FORGE_BUILD;
