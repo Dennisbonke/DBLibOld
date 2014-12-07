7e588cad4051c028a8fd500b501c27828c18acfe
Dennisbonke <dennisb@kabelfoon.net>
2014-12-07 11:42:01 +0100

    Wrapping up for release, version 0.3.0.

--- src/main/java/com/dennisbonke/dblib/reference/Reference.java
+++ src/main/java/com/dennisbonke/dblib/reference/Reference.java
@@ -9,7 +9,7 @@ public static final String MOD_NAME = "DBLib";
 // Versions
 public static final String MC_VERSION = "1.7.10";
 public static final String FORGE_BUILD = "10.13.0.1240";
-public static final int MAJOR = 0, MINOR = 1, REVISION = 0, BUILD = 1;
+public static final int MAJOR = 0, MINOR = 3, REVISION = 0, BUILD = 3;
 public static final String VERSION = MC_VERSION + "-" + MAJOR + "." + MINOR + "." + REVISION + "." + BUILD;
 public static final String FORGE_VERSION = MC_VERSION + "-" + FORGE_BUILD;
 
