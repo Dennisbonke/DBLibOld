a801624100c793be76080241eae0a78e7db9e973
Dennisbonke <dennisb@kabelfoon.net>
2014-12-15 17:00:57 +0100

    Added GUI Helpers.

---
+++ src/main/java/com/dennisbonke/dblib/gui/GuiControlListener.java
@@ -0,0 +1,12 @@
+package com.dennisbonke.dblib.gui;
+
+import net.minecraft.client.gui.Gui;
+
+/**
+ * Created by lukas on 28.05.14.
+ */
+public class GuiControlListener<G extends Gui>{
+    void valueChanged(G gui) {
+
+    }
+}
---
+++ src/main/java/com/dennisbonke/dblib/gui/IntegerRange.java
@@ -0,0 +1,43 @@
+package com.dennisbonke.dblib.gui;
+
+import net.minecraft.util.MathHelper;
+
+/**
+ * Created by lukas on 13.06.14.
+ */
+public class IntegerRange {
+    public final int min;
+    public final int max;
+
+    public IntegerRange(int min, int max)
+    {
+        this.min = min;
+        this.max = max;
+    }
+
+    @Deprecated
+    public IntegerRange(FloatRange floatRange)
+    {
+        min = MathHelper.floor_float(floatRange.getMin());
+        max = MathHelper.floor_float(floatRange.getMax());
+    }
+
+    public int getMin()
+    {
+        return min;
+    }
+
+    public int getMax()
+    {
+        return max;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "IntegerRange{" +
+                "min=" + min +
+                ", max=" + max +
+                '}';
+    }
+}
