50a858cad865e9a9f76f14599199699ca6aea5e8
Dennisbonke <dennisb@kabelfoon.net>
2014-12-26 11:13:09 +0100

    Added Block stuff.

---
+++ src/main/java/com/dennisbonke/dblib/blocks/IvRotatableBlockRenderHelper.java
@@ -0,0 +1,14 @@
+package com.dennisbonke.dblib.blocks;
+
+import org.lwjgl.opengl.GL11;
+
+/**
+ * Created by lukas on 26.07.14.
+ */
+public class IvRotatableBlockRenderHelper {
+    public static void transformFor(IvTileEntityRotatable tileEntity, double renderX, double renderY, double renderZ)
+    {
+        GL11.glTranslated(renderX + 0.5, renderY + 0.5, renderZ + 0.5);
+        GL11.glRotatef(-90.0f * tileEntity.direction + 180.0f, 0.0f, 1.0f, 0.0f);
+    }
+}
