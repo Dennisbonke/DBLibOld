183a32c0265f6b81380863bf646d22c6e89bee65
Dennisbonke <dennisb@kabelfoon.net>
2015-01-18 16:51:14 +0100

    Added inventory.

---
+++ src/main/java/com/dennisbonke/dblib/inventory/TransactorSpecial.java
@@ -0,0 +1,29 @@
+package com.dennisbonke.dblib.inventory;
+
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.common.util.ForgeDirection;
+
+public class TransactorSpecial extends Transactor{
+    protected ISpecialInventory inventory;
+
+    public TransactorSpecial(ISpecialInventory inventory) {
+        this.inventory = inventory;
+    }
+
+    @Override
+    public int inject(ItemStack stack, ForgeDirection orientation, boolean doAdd) {
+        return inventory.addItem(stack, doAdd, orientation);
+    }
+
+    @Override
+    public ItemStack remove(IStackFilter filter, ForgeDirection orientation, boolean doRemove) {
+        ItemStack[] extracted = inventory.extractItem(false, orientation, 1);
+        if (extracted != null && extracted.length > 0 && filter.matches(extracted[0])) {
+            if (doRemove) {
+                inventory.extractItem(true, orientation, 1);
+            }
+            return extracted[0];
+        }
+        return null;
+    }
+}
